612dfd70d838cd90408b12a7a9d97866e078e923
Jackson Ribeiro <jacksonribeiro@bemol.com.br>
2024-12-04 10:09:52 -0400

    Add findAllCharacter, findOneCharacter e toList busca de um ou todos os personagens e conversão para Lista.

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,3 +1,9 @@
+import 'dart:js_interop';
+
+import 'package:personagem_list/components/character_card.dart';
+import 'package:personagem_list/data/database.dart';
+import 'package:sqflite/sqflite.dart';
+
 class CharacterDao {
   // Add your SQL table creation query here
   static const String sqlTable = "CREATE TABLE $_characterTable("
@@ -12,4 +18,51 @@ class CharacterDao {
   static const String _race = "race";
   static const String _strength = "strength";
   static const String _image = "image";
+
+
+  saveCharacter(){
+
+  }
+
+  Future<List<CharacterCard>> findAllCharacter() async{
+    print("Acessando findAll...");
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_characterTable);
+    return toList(result);
+  }
+
+  Future<List<CharacterCard>> findOneCharacter(String characterName) async{
+    print("Acessando findOneCharacter...");
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_characterTable, where: "$_name = ?", whereArgs: [characterName]);
+    print("Personagem encontrado: $result");
+    return toList(result);
+  }
+
+  updateCharacter(){
+
+  }
+
+  deleteCharacter(){
+
+  }
+
+  List<CharacterCard> toList(List<Map<String, dynamic>> characterMap) {
+    print("Acessando o toList...");
+    final List<CharacterCard> characters = [];
+
+    print("Percorrendo o mapa de dados");
+    for(Map<String,dynamic> linha in characterMap){
+      final CharacterCard character = CharacterCard(
+          linha[_name],
+          linha[_race],
+          linha[_strength],
+          linha[_image]);
+      characters.add(character);
+    }
+    print("Lista de personagem $characterMap");
+    return characters;
+
+  }
+
 }
